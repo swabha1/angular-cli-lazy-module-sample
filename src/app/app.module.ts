f70abef8cad01b5a2b0743a63c790996abaeb809
Swapnil Bhamat <swapnil.bhamat@MRRSoft.com>
2017-08-10 16:45:13 +0530

    Implemented custom plugin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRouting } from './app.routing';
 import { SharedModule } from './shared/shared.module';
+import { SampleModule } from 'artifi-shared-component';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRouting,
-    SharedModule
+    SharedModule,
+    SampleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
